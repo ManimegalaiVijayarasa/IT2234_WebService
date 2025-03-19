b03af929a05615d38b1fa8678cebce19f65045bb
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-03-19 21:55:23 +0530

    Add files via upload

---
+++ ArrayOperations.js
@@ -0,0 +1,10 @@
+// 7. Array operation
+// push and pop
+
+console.log("------------- 7. Array operation---------------- ")
+arr3= ['a','b','d']
+console.log(arr3)
+arr3.push('e')
+console.log(arr3)
+arr3.pop()
+console.log(arr3)
