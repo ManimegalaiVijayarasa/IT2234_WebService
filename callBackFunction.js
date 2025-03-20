7614ebe0df42a36fefc6bbd2a00a390764b4c450
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-03-20 18:10:25 +0530

    Add files via upload

---
+++ callBackFunction.js
@@ -0,0 +1,18 @@
+
+// callback Function
+// a function passed as an argument
+
+const greet = (msg,fun)=> {
+	console.log("Hi.."+msg)
+	fun()
+}
+const myName = (name)=> {console.log("My name is  "+name)}
+greet("Good morning ", ()=>{console.log("My name is David")})
+
+
+const multtwo = (n) => n*2
+const myarr= (mul,...n) =>{
+	n.forEach((i)=>console.log(mul(i)))
+	
+}
+myarr(multtwo,4,5,6,8,2)
