b4420ce3d6318d6a3e77d1acced20817bd110d7b
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-03-25 13:28:10 +0530

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,9 @@
+let students =[
+    {regno:'2021ICT91', name:'Sathu', age:22, course:'IT', gender:'Female'},
+	{regno:'2021ICT92', name:'Rajeevni', age:22, course:'IT', gender:'Female'},
+	{regno:'2021ICT100', name:'Bavaruba', age:23, course:'AMC', gender:'Female'},
+    {regno:'2021ICT101', name:'Rajeev', age:23, course:'IT', gender:'Male'},
+    {regno:'2021ICT105', name:'Manimegalai', age:23, course:'IT', gender:'Female'}
+];
+
+module.exports=students;
