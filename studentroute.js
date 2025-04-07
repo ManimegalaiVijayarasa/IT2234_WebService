6d517c5c0ca66248105d1c0c2f5e3a0ac001af64
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-04-07 21:21:53 +0530

    Add files via upload

---
+++ studentroute.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const router = express.Router()
+const studentService = require('./studentservice')
+
+router.get('/',(req,res)=>{
+    const results = studentService.getstudents()
+    if(results){
+        res.status(200).json(results)
+    }else{
+        res.status(404).send("Sorry, No Data Found!")
+    }
+})  
+
+router.get('/:id',(req,res)=>{
+    const id = req.params.id
+    const results = studentService.getStudent(id)
+    if(results){
+        res.status(200).json(results)
+    }else{
+        res.status(404).send("Sorry, No Data Found!")
+    }
+    
+})
+
+//filter by gender
+router.get('/gender/:gen', (req, res)=>{
+    const gender=req.params.gen=='m'?'Male': 'Female'
+    //console.log(gender)
+    const results= studentService.getByGender(gender);
+    if(results){
+        res.status(200).json(results)
+    }else{
+        res.status(404).send("Sorry, No Data Found!")
+    }
+});
+
+module.exports=router
